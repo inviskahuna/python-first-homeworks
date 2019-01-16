8a1cb3e481358e03cd763198aa2fcdc005aad750
inviskahuna <rvlatyhev@gmail.com>
2019-01-16 19:40:54 +0300

    add numdat2strdat

--- lesson02/p_solution/hw02_normal.py
+++ lesson02/p_solution/hw02_normal.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from datetime import date
 
 # Задача-1:
 # Дан список, заполненный произвольными целыми числами, получите новый список,
@@ -23,6 +24,25 @@ def num_sqrts(num_list):
 # Ваша задача вывести дату в текстовом виде, например: второе ноября 2013 года.
 # Склонением пренебречь (2000 года, 2010 года)
 
+def numdat2strdat(numd):
+    #maybe can use num2words thirdparty library, now hardway with stdlib
+    d = str(numd).split('.')
+    di = []
+    for i in d:
+        di.append(int(i))
+    dd = date(int(di[2]),int(di[1]),int(di[0]))
+    A = (dd.strftime("It`s %B %dth %Y")) #Beuty
+    days = {"01":"first", "02":"second", "03":"third", "04":"fours", "05":"fifth",
+            "06":"sixth", "07":"seventh", "08":"eighth", "09":"nineth", "10":"tenth",
+            "11":"eleven", "12":"twelve", "13":"thirteenth", "14":"second", "15":"third",
+            "16":"sixteenth", "17":"seventeenth", "18":"eighteenth", "19":"nineteenth", "20":"twenty",
+            "21":"twenty one", "22":"twenty two", "23":"twenty three", "24":"twenty four", "25":"twenty five",
+            "26":"twenty six", "27":"twenty seven", "28":"twenty eight", "29":"twenty nine", "30":"thirty",
+            "31":"thirty one"}
+    B = (dd.strftime("It`s %B the {}, %Y").format(days[d[0]])) # ugly
+    return A, B
+
+        
 
 # Задача-3: Напишите алгоритм, заполняющий список произвольными целыми числами
 # в диапазоне от -100 до 100. В списке должно быть n - элементов.
@@ -40,7 +60,8 @@ def num_sqrts(num_list):
 def main():
     nums = [2, -5, 8, 9, -25, 25, 4]
     print(num_sqrts(nums))
-
+    data = "16.04.2013"
+    print(numdat2strdat(data))
 
 if __name__ == "__main__":
     try:
