ad5c42f056cecc3cfeed79f279af0c0e27c64353
inviskahuna <rvlatyhev@gmail.com>
2019-01-16 18:53:31 +0300

    easy case2 - list_xor

--- lesson02/p_solution/hw02_easy.py
+++ lesson02/p_solution/hw02_easy.py
@@ -31,6 +31,10 @@ def list2numlist(fruits_names):
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
 
+def list_xor(list1, list2):
+    for i in list2:
+        list1.remove(i)
+    return list1
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
@@ -41,7 +45,10 @@ def main():
     #Case 1
     fruits = ["apple", "banana", "kivi", "watermelon", "peach", "melon"]
     print(list2numlist(fruits))
-
+    #Case 2
+    a = [1,2,3,4,5,6,7,8,9]
+    b = [2,4,6,8]
+    print(list_xor(a,b))
 if __name__ == "__main__":
     try:
         main()
