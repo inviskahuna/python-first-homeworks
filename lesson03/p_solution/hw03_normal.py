b0c8df28f2bc2979864c869d6b6be9ae7a6d4e3b
inviskahuna <rvlatyhev@gmail.com>
2019-01-20 11:33:34 +0300

    add fib and sort to max

--- lesson03/p_solution/hw03_normal.py
+++ lesson03/p_solution/hw03_normal.py
@@ -1,9 +1,18 @@
+from random import choice
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
+
 def fibonacci(n, m):
-    pass
+    a = 0
+    b = 1
+    c = []
+# like a dynamic prog
+    for __ in range(m):
+        a, b = b, a + b
+        c.append(a)
+    return c[n:]
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -12,16 +21,32 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
-    pass
-
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+    # Reference: http://www.itmathrepetitor.ru/sortirovka-khoara-bystraya-sortirovka/
+    if len(origin_list) <= 1:
+        return origin_list
+    else:
+        seed = choice(origin_list)
+    l_nums = [n for n in origin_list if n < seed]
+    e_nums = [seed] * origin_list.count(seed)
+    b_nums = [n for n in origin_list if n > seed]
+    return sort_to_max(l_nums) + e_nums + sort_to_max(b_nums)
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
 
+
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+def main():
+    print(fibonacci(4, 32))
+    print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n exit")
