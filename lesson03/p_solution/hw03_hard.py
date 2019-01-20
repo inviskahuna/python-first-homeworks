349b2faa4eeac37e999c012b7ab876bc86fa988d
inviskahuna <rvlatyhev@gmail.com>
2019-01-20 18:36:55 +0300

    add salary calc

--- lesson03/p_solution/hw03_hard.py
+++ lesson03/p_solution/hw03_hard.py
@@ -1,4 +1,7 @@
 import re
+import os
+
+
 # Задание-1:
 # Написать программу, выполняющую операции (сложение и вычитание) с простыми дробями.
 # Дроби вводятся и выводятся в формате:
@@ -10,7 +13,8 @@ import re
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
 
-def my_calc(in_str:str):
+
+def my_calc(in_str: str):
     pattern = r'((\d+) (\d+/\d+) ([+,-]) (\d+) (\d+/\d+))'
     #         numr
     # integer-------
@@ -50,6 +54,59 @@ def my_calc(in_str:str):
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
 
 
+def salary():
+
+    def make_dict(w, h):
+        lw = []
+        lh = []
+        for i in w:
+            workers_dict = {"name": i[0], "l_name": i[1], "wage": i[2], "prof": i[3], "norm": i[4]}
+            lw.append(workers_dict)
+        for i in h:
+            hours_dict = {"name": i[0], "l_name": i[1], "hours": i[2]}
+            lh.append(hours_dict)
+        return lw, lh
+
+    def calc_salary(wage, norm, hours):
+        wage = int(wage)
+        norm = int(norm)
+        hours = int(hours)
+        if hours <= norm:
+            m_salary = wage * (hours / norm)
+        else:
+            e_hours = (hours - norm) * 2
+            m_salary = wage * (1 + (e_hours / norm))
+        return round(m_salary, 2)
+
+    workers = os.path.join("data", "workers")
+    hours_of = os.path.join("data", "hours_of")
+
+    pattern_w = r'(\w+\s+\w+\s+\d+\s+\w+\s+\d+)'
+    pattern_h = r'(\w+\s+\w+\s+\d+)'
+    workers_list = []
+    hours_list = []
+    # Read workers
+    with open(workers, 'r', encoding="UTF8-") as f:
+        r = re.findall(pattern_w, f.read())
+        for i in r:
+            workers_list.append(re.split("\s+", i))
+    # Read hours
+    with open(hours_of, 'r', encoding="UTF8-") as f:
+        r = re.findall(pattern_h, f.read())
+        for i in r:
+            hours_list.append(re.split("\s+", i))
+
+        dictionares = make_dict(workers_list, hours_list)
+        w_dict = dictionares[0]
+        h_dict = dictionares[1]
+        i = 0
+        sss = []
+        while i < len(w_dict):
+            sss.append({w_dict[i]["name"], w_dict[i]["l_name"], calc_salary(w_dict[i]['wage'], w_dict[i]['norm'], h_dict[i]["hours"])})
+            i += 1
+        return sss
+
+
 # Задание-3:
 # Дан файл ("data/fruits") со списком фруктов.
 # Записать в новые файлы все фрукты, начинающиеся с определенной буквы.
@@ -65,6 +122,7 @@ def my_calc(in_str:str):
 
 def main():
     print(my_calc("1 1/3 + 0 2/5"))
+    print(salary())
 
 
 if __name__ == "__main__":
