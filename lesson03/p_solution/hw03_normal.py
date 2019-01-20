1470549a6ce409d8f31a30b0ce4bbe7344cdc44f
inviskahuna <rvlatyhev@gmail.com>
2019-01-20 13:21:55 +0300

    add isparallelogramm

--- lesson03/p_solution/hw03_normal.py
+++ lesson03/p_solution/hw03_normal.py
@@ -1,4 +1,5 @@
 from random import choice
+from math import sqrt
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
@@ -36,14 +37,37 @@ def sort_to_max(origin_list):
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
 
+def my_filter(_func, _list):
+    ret = []
+    for i in _list:
+        if _func(i):
+            ret.append(i)
+    return ret
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+
+def isparallelogramm(A: (int, int), B: (int, int), C: (int, int), D: (int, int)):
+    # -------B===============C--
+    # -----/---------------/----
+    # ----/---------------/-----
+    # ---/---------------/------
+    # -A===============D--------
+    AB = sqrt((B[0] - A[0])**2 + (B[1] - A[1])**2)
+    DC = sqrt((C[0] - D[0]) ** 2 + (C[1] - D[1]) ** 2)
+    AD = sqrt((D[0] - A[0]) ** 2 + (D[1] - A[1]) ** 2)
+    BC = sqrt((C[0] - B[0]) ** 2 + (C[1] - B[1]) ** 2)
+    return True if AB == DC and AD == BC else False
+
+
 def main():
     print(fibonacci(4, 32))
     print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
+    print(my_filter(lambda x: x % 3 == 0, list(range(32))))
+    print(isparallelogramm((2, 3), (4, 11), (9, 11), (7, 3)))
+    print(isparallelogramm((2, 3), (4, 0), (9, 11), (7, 3)))
 
 if __name__ == "__main__":
     try:
