d409b76bda9df1ca270b4b10b3597ed9c6b4b22e
inviskahuna <rvlatyhev@gmail.com>
2019-01-23 13:10:17 +0300

    easy done

--- lesson04/p_solution/hw04_easy.py
+++ lesson04/p_solution/hw04_easy.py
@@ -8,11 +8,15 @@
 
 
 def quadratic_list(in_list):
-    return [x for x in in_list]
+    return [x**2 for x in in_list]
 
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+
+def fruits_lists(fruits1, fruits2):
+    return [x for x in fruits1 if x in fruits2]
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -22,9 +26,17 @@ def quadratic_list(in_list):
 # + Элемент не кратен 4
 
 
+def condition_list(in_list):
+    return [x for x in in_list if (x % 3 == 0) and (x % 4 != 0) and (x > 0)]
+
+
 def main():
     q_list = [1, 2, 4, 0]
     print(quadratic_list(q_list))
+    fr1 = ["melon", "apple", "watermelon", "kiwi"]
+    fr2 = ["apple", "banana", "peach", "kiwi"]
+    print(fruits_lists(fr1, fr2))
+    print(condition_list(range(-10, 40)))
 
 
 if __name__ == "__main__":
